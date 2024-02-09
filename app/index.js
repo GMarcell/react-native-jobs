a797e9a0ed0e072e5c49872f07b3b59ed25be349
Grand Marcell <grand1310marcell@gmail.com>
2024-02-09 18:38:04 +0700

    add starter file

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native"
+import {useRouter, Stack} from 'expo-router'
 
 const Home = () => {
+  const router = useRouter()
   return (
     <View>
       <Text>Hello</Text>
