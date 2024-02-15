2b6e455db360b97488cdc1820003576b49d4610a
Grand Marcell <grand1310marcell@gmail.com>
2024-02-15 22:51:21 +0700

    delete console.log

--- app/_layout.js
+++ app/_layout.js
@@ -14,7 +14,6 @@ const Layout = () => {
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
-      console.log('hello')
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded]);
