b50d7db3a65a53afe7d62b5633daebf6484b75d0
Grand Marcell <grand1310marcell@gmail.com>
2024-02-15 22:51:03 +0700

    handle error splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,34 @@
 import { Stack } from "expo-router";
-import {useFonts} from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
-import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect } from "react";
 
-SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
   const [fontsLoaded] = useFonts({
-    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-  })
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
   const onLayoutRootView = useCallback(async () => {
-    if(fontsLoaded){
-      await SplashScreen.hideAsync()
+    if (fontsLoaded) {
+      console.log('hello')
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  useEffect(() => {
+    if (fontsLoaded){
+      onLayoutRootView()
     }
   }, [fontsLoaded])
 
-  if(!fontsLoaded) return null
+  if (!fontsLoaded) return null;
 
-  return <Stack onLayout={onLayoutRootView}/>;
+  return <Stack/>;
+  // return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
